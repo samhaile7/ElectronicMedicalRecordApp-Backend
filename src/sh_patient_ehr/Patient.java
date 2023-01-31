fcab03a2985d2e3f20b6ade2ee4a51d8d2362a69
Samuel Haile <samhaile129@gmail.com>
2023-01-30 23:30:21 -0500

    Added Patient Attributes and Constructors

--- src/sh_patient_ehr/Patient.java
+++ src/sh_patient_ehr/Patient.java
@@ -1,4 +1,22 @@
 package sh_patient_ehr;
 
 public class Patient {
+
+    //Attributes
+
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    //Constructor
+
+    public Patient() {};
+
+    public Patient(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+
 }
