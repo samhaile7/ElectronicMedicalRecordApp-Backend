35447a9b850e34d2752159077405775672ad9331
Samuel Haile <samhaile129@gmail.com>
2023-02-05 22:58:42 -0500

    Added Rangechecker class

--- src/sh_patient_ehr/Main.java
+++ src/sh_patient_ehr/Main.java
@@ -2,6 +2,6 @@ package sh_patient_ehr;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("App will go here");
+        System.out.println("App goes here");
     }
 }
---
+++ src/sh_patient_ehr/RangeChecker.java
@@ -0,0 +1,9 @@
+package sh_patient_ehr;
+
+public class RangeChecker {
+
+    // Add min and max lab values here, final constants
+    //construct using patient id and lab values
+    //compare lab values to constant ranges, set mobility status
+
+}
