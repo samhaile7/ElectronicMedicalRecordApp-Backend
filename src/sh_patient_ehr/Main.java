51ce46d3fe390011320ec83a8a9d11c55e5c5882
Samuel Haile <samhaile129@gmail.com>
2023-02-05 22:43:25 -0500

    Added patient id number

--- src/sh_patient_ehr/Main.java
+++ src/sh_patient_ehr/Main.java
@@ -2,6 +2,6 @@ package sh_patient_ehr;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("App will go here");
     }
 }
--- src/sh_patient_ehr/Patient.java
+++ src/sh_patient_ehr/Patient.java
@@ -7,15 +7,17 @@ public class Patient {
     private String firstName;
     private String lastName;
     private int age;
+    private int idNumber;
 
     //Constructor
 
     public Patient() {};
 
-    public Patient(String firstName, String lastName, int age) {
+    public Patient(String firstName, String lastName, int age, int idNumber) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.age = age;
+        this.idNumber = idNumber;
     }
 
     // Add getters and setters once added all attributes (labs especially)
