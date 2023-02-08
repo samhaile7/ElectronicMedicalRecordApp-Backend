afbbd266c2e011b51219425424418805b57f142c
Samuel Haile <samhaile129@gmail.com>
2023-02-07 21:35:16 -0500

    Added lab value ranges

--- src/sh_patient_ehr/RangeChecker.java
+++ src/sh_patient_ehr/RangeChecker.java
@@ -3,6 +3,12 @@ package sh_patient_ehr;
 public class RangeChecker {
 
     // Add min and max lab values here, final constants
+    final double MIN_INR = 0;
+    final double MAX_INR = 0;
+    final double MIN_BP = 0;
+    final double MAX_BP = 0;
+    final double MIN_PTT = 0;
+    final double MAX_PTT = 0;
     //construct using patient id and lab values
     //compare lab values to constant ranges, set mobility status
 
