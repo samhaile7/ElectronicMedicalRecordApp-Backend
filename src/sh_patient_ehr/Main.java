dce1753041ddf00465b004d3412e3a0715ad5131
Samuel Haile <samhaile129@gmail.com>
2023-02-07 22:04:45 -0500

    Added patient dashboard brainstorm

--- src/sh_patient_ehr/Main.java
+++ src/sh_patient_ehr/Main.java
@@ -3,5 +3,20 @@ package sh_patient_ehr;
 public class Main {
     public static void main(String[] args) {
         System.out.println("App goes here");
+
+
+        //Display Welcome! here is a list of patients: and their details
+        //Display Master array list
+        // display time
+        //What would you like to do?
+        // 1 update record
+        // 2 Get mobility status
+        // 3 discharge (are you sure?) then add a discharged
+        // 4 add new patient
+        // 5 view patient dashboard
+            // enter name, last name etc.and add to constructor
+            // print success! Patient created: you will find them on patient dashboard
+        // 4 logger system with time functionality into a text file
+
     }
 }
