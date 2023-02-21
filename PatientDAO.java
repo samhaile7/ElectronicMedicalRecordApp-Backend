7f7a5c80ff4ecb4b5f3c502fdf08a1daaa772384
samhaile7 <110855004+samhaile7@users.noreply.github.com>
2023-02-21 09:15:08 -0500

    Created DAO class

---
+++ PatientDAO.java
@@ -0,0 +1,4 @@
+// This class creates and updates new patients in database. 
+// method get Patient(patientid) that reads from database and returns patient object
+// from sql row to customer object private method
+//insert method that inserts into databsae, returning patient id, then set that new id to patient object with returning
