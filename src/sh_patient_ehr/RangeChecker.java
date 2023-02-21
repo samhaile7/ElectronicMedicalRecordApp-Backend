a686c3a9eeee94add070bf968a93e92e6df0fe1f
samhaile7 <110855004+samhaile7@users.noreply.github.com>
2023-02-21 08:57:23 -0500

    Added more patient vitals properties

--- src/sh_patient_ehr/RangeChecker.java
+++ src/sh_patient_ehr/RangeChecker.java
@@ -3,12 +3,14 @@ package sh_patient_ehr;
 public class RangeChecker {
 
     // Add min and max lab values here, final constants
-    final double MIN_INR = 0;
-    final double MAX_INR = 0;
-    final double MIN_BP = 0;
-    final double MAX_BP = 0;
+    final double MIN_SBP = 0;
+    final double MAX_SBP = 0;
+    final double MIN_DBP = 0;
+    final double MAX_DBP = 0;
     final double MIN_PTT = 0;
     final double MAX_PTT = 0;
+    final double MIN_HR = 0;
+    final double MAX_HR = 0;
     //construct using patient id and lab values
 
     //update mobility status method(patient) with patient as a parameter
