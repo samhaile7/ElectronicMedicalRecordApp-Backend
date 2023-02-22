ea28a3dd03b0b01af8b90e7868f49f8cdd9be4c8
Samuel Haile <samhaile129@gmail.com>
2023-02-21 22:09:17 -0500

    Added todos for UI

--- src/sh_patient_ehr/Main.java
+++ src/sh_patient_ehr/Main.java
@@ -9,11 +9,11 @@ public class Main {
         //Display Master array list
         // display time
         //What would you like to do?
-        // 1 update record
+        // 1 update record (usa Patient dao to update record in database)
         // 2 Get mobility status
         // 3 discharge (are you sure?) then add a discharged
-        // 4 add new patient
-        // 5 view patient dashboard
+        // 4 add new patient (create new patient object, pass it to PAtient DAO interface to add to SQL table)
+        // 5 view patient dashboard (uses patient DAo to show current list of patients)
             // enter name, last name etc.and add to constructor
             // print success! Patient created: you will find them on patient dashboard
         // 4 logger system with time functionality into a text file
