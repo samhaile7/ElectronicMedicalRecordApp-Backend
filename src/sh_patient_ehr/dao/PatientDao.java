90b0d37ed472e6bca567464000183ef49b0ed40d
Samuel Haile <samhaile129@gmail.com>
2023-02-21 22:29:41 -0500

    Implemented PatientDao interface methods

--- src/sh_patient_ehr/dao/PatientDao.java
+++ src/sh_patient_ehr/dao/PatientDao.java
@@ -1,4 +1,32 @@
 package sh_patient_ehr.dao;
 
+import sh_patient_ehr.Patient;
+
 public class PatientDao implements PatientDaoInterface{
+
+
+    @Override
+    public Patient createPatient(Patient patient) {
+        return null;
+    }
+
+    @Override
+    public void removePatient(Patient patient) {
+
+    }
+
+    @Override
+    public void updatePatient(Patient patient) {
+
+    }
+
+    @Override
+    public void getCurrentListPatients() {
+
+    }
+
+    @Override
+    public Patient getPatientById(int Id) {
+        return null;
+    }
 }
