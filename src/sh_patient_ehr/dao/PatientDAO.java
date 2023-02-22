aa69e45e6aeb77bdd712b6390f52c3d44fa6b89e
Samuel Haile <samhaile129@gmail.com>
2023-02-21 21:49:15 -0500

    Created DAO interface

---
+++ src/sh_patient_ehr/dao/PatientDAO.java
@@ -0,0 +1,12 @@
+package sh_patient_ehr.dao;
+
+public interface PatientDAO {
+
+    //TODO Create/Add patient to datasource
+
+    //TODO Remove patient from datasource
+
+    //TODO Update Patient Info
+
+    //TODO Read patient/show list of patients, or get patient by ID
+}
