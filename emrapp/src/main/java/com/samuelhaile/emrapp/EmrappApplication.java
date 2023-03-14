a76e608703a69d3cf6b7f2126b45c4290579550a
Samuel Haile <samhaile129@gmail.com>
2023-03-13 23:57:48 -0400

    Created Spring Boot App

---
+++ emrapp/src/main/java/com/samuelhaile/emrapp/EmrappApplication.java
@@ -0,0 +1,13 @@
+package com.samuelhaile.emrapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmrappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmrappApplication.class, args);
+	}
+
+}
---
+++ emrapp/src/test/java/com/samuelhaile/emrapp/EmrappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samuelhaile.emrapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmrappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
