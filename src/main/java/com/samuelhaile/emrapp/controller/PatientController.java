7a865e495adb4fc1c684d66a36ad2ed5d104d123
Samuel Haile <samhaile129@gmail.com>
2023-03-19 18:55:07 -0400

    Created PatientController

--- src/main/java/com/samuelhaile/emrapp/controller/PatientController.java
+++ src/main/java/com/samuelhaile/emrapp/controller/PatientController.java
@@ -1,8 +1,13 @@
 package com.samuelhaile.emrapp.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PatientController {
 
+@RequestMapping ("/testingpatient")
+    public String getTestMessage() {
+        return "This is a message that should display";
+    }
 }
