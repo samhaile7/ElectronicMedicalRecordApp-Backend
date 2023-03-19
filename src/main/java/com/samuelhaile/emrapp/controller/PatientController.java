45b4ca686a877871b2ac6aecf2d5d4d9e09ae687
Samuel Haile <samhaile129@gmail.com>
2023-03-19 18:59:38 -0400

    Added general path and method request

--- src/main/java/com/samuelhaile/emrapp/controller/PatientController.java
+++ src/main/java/com/samuelhaile/emrapp/controller/PatientController.java
@@ -1,12 +1,14 @@
 package com.samuelhaile.emrapp.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping (path ="/patient")
 public class PatientController {
 
-@RequestMapping ("/testingpatient")
+@RequestMapping (path = "/testingpatient", method = RequestMethod.GET)
     public String getTestMessage() {
         return "This is a message that should display";
     }
