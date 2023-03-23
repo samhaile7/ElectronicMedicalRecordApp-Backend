aa9946f5e337233744eb07b246ac1a93136ec8e9
Samuel Haile <samhaile129@gmail.com>
2023-03-22 22:57:13 -0400

    Completed Note Model

--- src/main/java/com/samuelhaile/emrapp/model/Note.java
+++ src/main/java/com/samuelhaile/emrapp/model/Note.java
@@ -9,4 +9,54 @@ public class Note {
     private String noteDetails;
     private LocalDate dateNoteAdded;
 
+    public Note() {
+    }
+
+    public Note(int patientId, String noteDetails, LocalDate dateNoteAdded) {
+        this.patientId = patientId;
+        this.noteDetails = noteDetails;
+        this.dateNoteAdded = dateNoteAdded;
+    }
+
+    public int getNoteId() {
+        return noteId;
+    }
+
+    public void setNoteId(int noteId) {
+        this.noteId = noteId;
+    }
+
+    public int getPatientId() {
+        return patientId;
+    }
+
+    public void setPatientId(int patientId) {
+        this.patientId = patientId;
+    }
+
+    public String getNoteDetails() {
+        return noteDetails;
+    }
+
+    public void setNoteDetails(String noteDetails) {
+        this.noteDetails = noteDetails;
+    }
+
+    public LocalDate getDateNoteAdded() {
+        return dateNoteAdded;
+    }
+
+    public void setDateNoteAdded(LocalDate dateNoteAdded) {
+        this.dateNoteAdded = dateNoteAdded;
+    }
+
+    @Override
+    public String toString() {
+        return "Note{" +
+                "noteId=" + noteId +
+                ", patientId=" + patientId +
+                ", noteDetails='" + noteDetails + '\'' +
+                ", dateNoteAdded=" + dateNoteAdded +
+                '}';
+    }
 }
