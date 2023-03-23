afc20247d78fbf52a73cece85f13c4035a7ea126
Samuel Haile <samhaile129@gmail.com>
2023-03-23 00:28:11 -0400

    Added listAll controller method

--- src/main/java/com/samuelhaile/emrapp/controller/PatientController.java
+++ src/main/java/com/samuelhaile/emrapp/controller/PatientController.java
@@ -1,15 +1,30 @@
 package com.samuelhaile.emrapp.controller;
 
+import com.samuelhaile.emrapp.dao.JdbcPatientDao;
+import com.samuelhaile.emrapp.dao.PatientDao;
+import com.samuelhaile.emrapp.model.Patient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping (path ="/patient")
 public class PatientController {
 
-@RequestMapping (path = "/testingpatient", method = RequestMethod.GET)
+    private PatientDao patientDao;
+
+    public PatientController(PatientDao patientDao) {
+        this.patientDao = patientDao;
+    }
+
+    @RequestMapping (path = "/testingpatient", method = RequestMethod.GET)
     public String getTestMessage() {
         return "This is a message that should display";
     }
+
+
+    @RequestMapping (path = "/listallpatients", method = RequestMethod.GET)
+    public List<Patient> listAllPatients() {return patientDao.listAllPatients();}
 }
