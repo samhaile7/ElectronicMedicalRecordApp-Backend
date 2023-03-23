182851a872a69381676cfe3bd86842b8eb352639
Samuel Haile <samhaile129@gmail.com>
2023-03-22 22:50:14 -0400

    Added Note Model

---
+++ src/main/java/com/samuelhaile/emrapp/model/Note.java
@@ -0,0 +1,12 @@
+package com.samuelhaile.emrapp.model;
+
+import java.time.LocalDate;
+
+public class Note {
+
+    private int noteId;
+    private int patientId;
+    private String noteDetails;
+    private LocalDate dateNoteAdded;
+
+}
