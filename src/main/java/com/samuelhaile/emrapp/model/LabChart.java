211d43b17b940e8ed793625716abd3e0e3ee11fd
Samuel Haile <samhaile129@gmail.com>
2023-03-22 22:49:54 -0400

    Added Labchart Model

---
+++ src/main/java/com/samuelhaile/emrapp/model/LabChart.java
@@ -0,0 +1,14 @@
+package com.samuelhaile.emrapp.model;
+
+public class LabChart {
+
+    private int labChartId;
+    private int patientId;
+    private int pulseRate;
+    private int respirationRate;
+    private int temperature;
+    private int sPO2;
+    private int partialThromboplastinTime;
+    private int systolicBloodPressure;
+    private int diastolicBloodPressure;
+}
