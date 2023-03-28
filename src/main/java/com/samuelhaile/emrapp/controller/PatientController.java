49dadb05d6946499519d6600e26d4c27d27ae143
Samuel Haile <samhaile129@gmail.com>
2023-03-27 21:24:09 -0400

    Added getPatientbyId controller method

--- src/main/java/com/samuelhaile/emrapp/controller/PatientController.java
+++ src/main/java/com/samuelhaile/emrapp/controller/PatientController.java
@@ -3,14 +3,17 @@ package com.samuelhaile.emrapp.controller;
 import com.samuelhaile.emrapp.dao.JdbcPatientDao;
 import com.samuelhaile.emrapp.dao.PatientDao;
 import com.samuelhaile.emrapp.model.Patient;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
 @RestController
-@RequestMapping (path ="/patient")
+
 public class PatientController {
 
     private PatientDao patientDao;
@@ -19,12 +22,25 @@ public class PatientController {
         this.patientDao = patientDao;
     }
 
-    @RequestMapping (path = "/testingpatient", method = RequestMethod.GET)
+    @RequestMapping(path = "/test", method = RequestMethod.GET)
     public String getTestMessage() {
         return "This is a message that should display";
     }
 
 
-    @RequestMapping (path = "/listallpatients", method = RequestMethod.GET)
-    public List<Patient> listAllPatients() {return patientDao.listAllPatients();}
+    @RequestMapping(path = "/patients", method = RequestMethod.GET)
+    public List<Patient> listAllPatients() {
+        return patientDao.listAllPatients();
+    }
+
+
+    @RequestMapping(path = "/patients/{id}", method = RequestMethod.GET)
+    public Patient getPatientById(@PathVariable int id) {
+        Patient patient = patientDao.getPatientById(id);
+        if (patient == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Patient not found");
+        } else {
+            return patient;
+        }
+    }
 }
