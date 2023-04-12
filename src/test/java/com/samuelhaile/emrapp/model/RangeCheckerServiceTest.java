ffa4f14d63302b1fdac24cc9ce86206ad998996f
Samuel Haile <samhaile129@gmail.com>
2023-04-11 20:37:55 -0400

    Added unit tests for range checker

---
+++ src/test/java/com/samuelhaile/emrapp/model/RangeCheckerServiceTest.java
@@ -0,0 +1,56 @@
+package com.samuelhaile.emrapp.model;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RangeCheckerServiceTest {
+
+    private RangeCheckerService rangeChecker;
+    private Patient testPatient;
+    private int CLEAR_FOR_MOBILITY_STATUS_ID = 1;
+    private int NOT_CLEAR_FOR_MOBILITY_STATUS_ID = 2;
+
+    @BeforeEach
+    void createsRangeCheckerAndNewPatient() {
+
+    this.rangeChecker =new RangeCheckerService();
+    this.testPatient = new Patient("John", "Doe",
+            LocalDate.of(2000,04,03),
+            LocalDate.of(2022,01,03), 60,
+            20, 98,99,
+            15,100,
+            80, 0   );
+
+
+}
+
+    @Test
+
+    void assigned_mobility_status_is_clear_with_stats_in_range() {
+        Patient outputPatient = rangeChecker.assignPatientMobilityStatus(testPatient);
+        assertEquals(CLEAR_FOR_MOBILITY_STATUS_ID, outputPatient.getMobilityStatusId(),
+                "Should assign mobility id 1 when values are in range");
+    };
+
+    @Test
+    void assigned_mobility_status_is_notclear_with_stats_in_out_of_range() {
+        testPatient.setSystolicBloodPressure(200);
+        Patient outputPatient = rangeChecker.assignPatientMobilityStatus(testPatient);
+
+        assertEquals(NOT_CLEAR_FOR_MOBILITY_STATUS_ID, outputPatient.getMobilityStatusId(),
+                "Should assign mobility id 2 when values are out of range");
+    };
+
+
+
+
+
+
+
+}
+
