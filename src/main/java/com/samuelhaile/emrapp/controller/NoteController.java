6074c168e636f3e12c0da6cb771e47da00b71097
Samuel Haile <samhaile129@gmail.com>
2023-04-11 23:08:07 -0400

    Completed note controller

--- src/main/java/com/samuelhaile/emrapp/controller/NoteController.java
+++ src/main/java/com/samuelhaile/emrapp/controller/NoteController.java
@@ -3,7 +3,6 @@ package com.samuelhaile.emrapp.controller;
 
 import com.samuelhaile.emrapp.dao.NoteDao;
 import com.samuelhaile.emrapp.model.Note;
-import com.samuelhaile.emrapp.model.Patient;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -44,6 +43,23 @@ public class NoteController {
     @RequestMapping(path = "/notes", method = RequestMethod.POST)
     public Note createNote(@RequestBody @Valid Note newNote) {
         return noteDao.createNote(newNote);
+    }
+
+    @RequestMapping(path = "/notes", method = RequestMethod.PUT)
+    public void updateNote( @RequestBody @Valid  Note noteToUpdate) {
+        if (noteDao.getNoteByNoteId(noteToUpdate.getNoteId()) == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Note Not Found");
+        }
+        noteDao.updateNote(noteToUpdate);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @RequestMapping(path = "/notes/{noteId}", method = RequestMethod.DELETE)
+    public void deleteNote( @PathVariable int noteId) {
+        if (noteDao.getNoteByNoteId(noteId) == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Patient Not Found");
+        }
+        noteDao.deleteNote(noteId);
     }
 
 }
