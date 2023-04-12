34aeb59cec967bf4bde2a79e028424e8f142b4ed
Samuel Haile <samhaile129@gmail.com>
2023-04-11 22:23:17 -0400

    Implementd getNoteByNoteId dao method

---
+++ src/main/java/com/samuelhaile/emrapp/dao/JdbcNoteDao.java
@@ -0,0 +1,66 @@
+package com.samuelhaile.emrapp.dao;
+
+import com.samuelhaile.emrapp.model.Note;
+import com.samuelhaile.emrapp.model.Patient;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+
+@Component
+public class JdbcNoteDao implements NoteDao {
+
+    private JdbcTemplate jdbcTemplate;
+
+    public JdbcNoteDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public List<Note> listAllNotesByPatientId(int patientId) {
+
+        return null;
+    }
+
+    @Override
+    public Note getNoteByNoteId(int noteId) {
+        Note note = null;
+        String sql =
+                "SELECT note_id, patient_id, note_details, date_added  " +
+                        "                        FROM patient_note " +
+                        "                        WHERE note_id = ?; ";
+        SqlRowSet results = jdbcTemplate.queryForRowSet(sql, noteId);
+
+        if (results.next()) {
+            note = mapRowToNote(results);
+        }
+        return note;
+    }
+
+    @Override
+    public Note createNote(Note note) {
+        return null;
+    }
+
+    @Override
+    public void deleteNote(int noteId) {
+
+    }
+
+    @Override
+    public void update(Note note) {
+
+    }
+
+    private Note mapRowToNote(SqlRowSet row) {
+        Note note = new Note();
+        note.setNoteId(row.getInt("note_id"));
+        note.setPatientId(row.getInt("patient_id"));
+        note.setNoteDetails(row.getString("note_details"));
+        note.setDateNoteAdded(row.getDate("date_added").toLocalDate());
+
+        return note;
+    }
+}
