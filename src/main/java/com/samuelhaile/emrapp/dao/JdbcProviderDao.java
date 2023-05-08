85382fae587ef51a1155df338fb4396ef03cc255
Samuel Haile <110855004+samhaile7@users.noreply.github.com>
2023-05-08 14:06:45 -0400

    Added two reads for provider daos

---
+++ src/main/java/com/samuelhaile/emrapp/dao/JdbcProviderDao.java
@@ -0,0 +1,85 @@
+package com.samuelhaile.emrapp.dao;
+
+import com.samuelhaile.emrapp.model.Patient;
+import com.samuelhaile.emrapp.model.Provider;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class JdbcProviderDao implements ProviderDao {
+
+    private JdbcTemplate jdbcTemplate;
+
+    @Override
+    public List<Provider> listAllProviders() {
+        List<Provider> listOfProviders = new ArrayList<>();
+
+
+        String sql = "SELECT provider_id, first_name, last_name, job_title " +
+                "FROM provider; ";
+
+        SqlRowSet results = jdbcTemplate.queryForRowSet(sql);
+
+        while (results.next()) {
+            listOfProviders.add(mapRowToProvider(results));
+
+        }
+        return listOfProviders;
+
+    }
+
+    @Override
+    public Provider getProviderById(int providerId) {
+
+        Provider provider = null;
+
+        String sql =
+                "SELECT provider_id, first_name, last_name, job_title " +
+                        "                FROM provider   " +
+                        "                WHERE provider_id = ?; ";
+
+
+        SqlRowSet results = jdbcTemplate.queryForRowSet(sql, providerId);
+
+        if (results.next()) {
+            provider = mapRowToProvider(results);
+        }
+        return provider;
+    }
+
+    @Override
+    public Provider createProvider(Provider provider) {
+        return null;
+    }
+
+    @Override
+    public void deleteProvider(int providerId) {
+
+    }
+
+    @Override
+    public void updateProvider(Provider provider) {
+
+    }
+
+    @Override
+    public List<Patient> listAllPatientsUnderProvider() {
+        return null;
+    }
+
+
+
+    private Provider mapRowToProvider(SqlRowSet row) {
+        Provider provider = new Provider();
+
+        provider.setProviderId(row.getInt("provider_id"));
+        provider.setFirstName(row.getString("first_name"));
+        provider.setLastName(row.getString("last_name"));
+        provider.setJobTitle(row.getString("job_title"));
+
+        return provider;
+
+    }
+}
