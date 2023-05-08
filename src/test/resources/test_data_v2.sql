0d2ab45f905d633a8823d866fc1436b2536ea9da
Samuel Haile <110855004+samhaile7@users.noreply.github.com>
2023-05-08 17:37:40 -0400

    Update schema with new job title table and mock data

--- src/test/resources/test_data_v2.sql
+++ src/test/resources/test_data_v2.sql
@@ -1,16 +1,23 @@
 DROP TABLE IF EXISTS mobility_status CASCADE;
+DROP TABLE IF EXISTS job_title CASCADE;
 DROP TABLE IF EXISTS patient_provider CASCADE;
 DROP TABLE IF EXISTS provider CASCADE;
 DROP TABLE IF EXISTS patient CASCADE;
 DROP TABLE IF EXISTS patient_note CASCADE;
 
+CREATE TABLE job_title (
+job_title_id serial,
+job_title_name varchar(50), 
+
+	 
+CONSTRAINT PK_job_title PRIMARY KEY (job_title_id));
 
 
 CREATE TABLE mobility_status (
 mobility_status_id serial,
-mobility_status_description varchar(10),
+mobility_status_description varchar(10), 
 
-
+	 
 CONSTRAINT PK_mobility_status PRIMARY KEY (mobility_status_id));
 
 
@@ -19,7 +26,7 @@ CONSTRAINT PK_mobility_status PRIMARY KEY (mobility_status_id));
 CREATE TABLE patient (
 patient_id serial,
 first_name varchar(100)NOT NULL,
-last_name varchar(100) NOT NULL,
+last_name varchar(100) NOT NULL, 
 birth_date date NOT NULL,
 admit_date date NOT NULL,
 pulse_rate int NOT NULL,
@@ -28,11 +35,11 @@ systolic_bp int NOT NULL,
 diastolic_bp int NOT NULL,
 sp_O2 int NOT NULL,
 temperature int NOT NULL,
-partial_thromboplastin_time int NOT NULL,
+partial_thromboplastin_time int NOT NULL, 
 mobility_status_id int NOT NULL,
+	
 
-
-CONSTRAINT PK_patient PRIMARY KEY (patient_id),
+CONSTRAINT PK_patient PRIMARY KEY (patient_id), 
 CONSTRAINT FK_patient_mobility_status FOREIGN KEY (mobility_status_id) REFERENCES mobility_status (mobility_status_id));
 
 
@@ -45,14 +52,16 @@ CONSTRAINT PK_patient_note PRIMARY KEY (note_id),
 CONSTRAINT FK_patient_note_patient FOREIGN KEY (patient_id) REFERENCES patient (patient_id));
 
 
+
 CREATE TABLE provider (
 provider_id serial,
 first_name varchar(100)NOT NULL,
-last_name varchar(100) NOT NULL,
-job_title varchar(100) NOT NULL,
+last_name varchar(100) NOT NULL, 
+job_title_id int NOT NULL,
+	
+	CONSTRAINT PK_provider PRIMARY KEY (provider_id),
+	CONSTRAINT FK_provider_job_title FOREIGN KEY (job_title_id) REFERENCES job_title (job_title_id));
 
-	CONSTRAINT PK_provider PRIMARY KEY (provider_id)
-	);
 
 CREATE TABLE patient_provider(
 	patient_provider_id serial NOT NULL,
@@ -61,10 +70,15 @@ CREATE TABLE patient_provider(
 	CONSTRAINT PK_patient_provider PRIMARY KEY (patient_provider_id),
 	CONSTRAINT FK_patient_provider_patient FOREIGN KEY(patient_id) REFERENCES patient (patient_id),
 	CONSTRAINT FK_patient_provider_provider FOREIGN KEY(provider_id) REFERENCES provider (provider_id));
-
+	
 
 INSERT INTO mobility_status (mobility_status_description) VALUES ('Clear');
 INSERT INTO mobility_status (mobility_status_description) VALUES ('Not Clear');
+
+INSERT INTO job_title (job_title_name) VALUES ('Physician');
+INSERT INTO job_title (job_title_name) VALUES ('Nurse');
+INSERT INTO job_title (job_title_name) VALUES ('Occupational Therapist');
+INSERT INTO job_title (job_title_name) VALUES ('Physical Therapist');
 
 INSERT INTO patient (first_name, last_name, birth_date, admit_date, pulse_rate, respiration_rate, systolic_bp, diastolic_bp, sp_O2, temperature, partial_thromboplastin_time, mobility_status_id)
 VALUES ('John', 'Doe', '1994-01-01', '2023-01-01', 54, 50, 120, 80, 99, 97, 1, 1);
@@ -78,11 +92,11 @@ VALUES (1, 'This is a note', '2023-03-14');
 INSERT INTO patient_note (patient_id , note_details , date_added )
 VALUES (2, 'This is a note', '2023-03-15');
 
-INSERT INTO provider (first_name, last_name, job_title)
-VALUES ('Serena', 'Williams', 'Physician') RETURNING provider_id;
+INSERT INTO provider (provider_id, first_name, last_name, job_title_id)
+VALUES (1, 'Serena', 'Williams', 3);
 
-INSERT INTO provider (first_name, last_name, job_title)
-VALUES ('Venus', 'Williams', 'Nurse') RETURNING provider_id;
+INSERT INTO provider (provider_id, first_name, last_name, job_title_id)
+VALUES (2, 'Venus', 'Williams', 2);
 
 
 
