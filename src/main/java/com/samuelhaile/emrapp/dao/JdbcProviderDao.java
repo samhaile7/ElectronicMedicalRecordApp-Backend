5d63da846a5e90810df978b83fb60c49d290c25c
Samuel Haile <110855004+samhaile7@users.noreply.github.com>
2023-05-12 18:39:16 -0400

    Fixed SQL grammar provider

--- src/main/java/com/samuelhaile/emrapp/dao/JdbcProviderDao.java
+++ src/main/java/com/samuelhaile/emrapp/dao/JdbcProviderDao.java
@@ -15,8 +15,10 @@ public class JdbcProviderDao implements ProviderDao {
     private JdbcTemplate jdbcTemplate;
     private JdbcPatientDao patientDao;
 
-    public JdbcProviderDao(JdbcPatientDao patientDao) {
+    public JdbcProviderDao(JdbcPatientDao patientDao,JdbcTemplate jdbcTemplate ) {
+
         this.patientDao = patientDao;
+        this.jdbcTemplate = jdbcTemplate;
     }
 
     @Override
@@ -102,13 +104,21 @@ public class JdbcProviderDao implements ProviderDao {
             List<Patient> listOfPatientsUnderProvider = new ArrayList<>();
 
 
-            String sql = "SELECT patient_id, first_name, last_name, birth_date, admit_date, pulse_rate, " +
-                    "  respiration_rate, systolic_bp, diastolic_bp, sp_O2, temperature, " +
-                    " partial_thromboplastin_time, mobility_status_id  " +
-                    "FROM patient" +
-                    "JOIN patient_provider " +
-                    "ON patient.patient_id = patient_provider.patient_id" +
-                    "WHERE patient_provider.provider_id = ?; ";
+//            String sql = "SELECT patient.patient_id, first_name, last_name, birth_date, admit_date, pulse_rate, " +
+//                    "  respiration_rate, systolic_bp, diastolic_bp, sp_O2, temperature, " +
+//                    " partial_thromboplastin_time, mobility_status_id  " +
+//                    "FROM patient" +
+//                    "JOIN patient_provider " +
+//                    "ON patient.patient_id = patient_provider.patient_id" +
+//                    "WHERE patient_provider.provider_id = ?; ";
+
+            String sql ="SELECT patient.patient_id, first_name, last_name, birth_date, admit_date, pulse_rate,  " +
+                    "                      respiration_rate, systolic_bp, diastolic_bp, sp_O2, temperature,  " +
+                    "                     partial_thromboplastin_time, mobility_status_id   " +
+                    "                    FROM patient " +
+                    "                    JOIN patient_provider  " +
+                    "                    ON patient.patient_id = patient_provider.patient_id " +
+                    "                    WHERE patient_provider.provider_id = ?; ";
 
             SqlRowSet results = jdbcTemplate.queryForRowSet(sql, providerId);
 
