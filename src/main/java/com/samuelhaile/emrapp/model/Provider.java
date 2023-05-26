caa7c58e751f7d5ea319505c3b84dab2891b9c99
Samuel Haile <110855004+samhaile7@users.noreply.github.com>
2023-05-26 09:50:18 -0400

    Changed to entity class

--- src/main/java/com/samuelhaile/emrapp/model/Provider.java
+++ src/main/java/com/samuelhaile/emrapp/model/Provider.java
@@ -1,18 +1,31 @@
 package com.samuelhaile.emrapp.model;
 
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 
+@Entity(name = "Provider")
 public class Provider {
-
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int providerId;
-    @NotBlank(message = "First name required")
+    @Column(nullable = false)
     private String firstName;
-    @NotBlank (message = "Last name required")
+    @Column(nullable = false)
     private String lastName;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false)
     private int jobTitleId;
 
     public Provider() {
+    }
+
+    public Provider(int providerId, String firstName, String lastName, String email, int jobTitleId) {
+        this.providerId = providerId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.jobTitleId = jobTitleId;
     }
 
     public Provider(String firstName, String lastName, int jobTitleId) {
@@ -45,11 +58,30 @@ public class Provider {
         this.lastName = lastName;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     public int getJobTitleId() {
         return jobTitleId;
     }
 
     public void setJobTitleId(int jobTitleId) {
         this.jobTitleId = jobTitleId;
+    }
+
+    @Override
+    public String toString() {
+        return "Provider{" +
+                "providerId=" + providerId +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", jobTitleId=" + jobTitleId +
+                '}';
     }
 }
