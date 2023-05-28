05f8f73b14db15f37a0310f9af9b6d164223919b
Samuel Haile <110855004+samhaile7@users.noreply.github.com>
2023-05-28 19:17:46 -0400

    Updated Entity relationship job

--- src/main/java/com/samuelhaile/emrapp/model/Job.java
+++ src/main/java/com/samuelhaile/emrapp/model/Job.java
@@ -2,6 +2,8 @@ package com.samuelhaile.emrapp.model;
 
 import jakarta.persistence.*;
 
+import java.util.List;
+
 @Entity(name = "Job")
 public class Job {
     @Id
@@ -9,5 +11,9 @@ public class Job {
     private Long jobId;
     @Column (nullable = false)
     private String jobTitle;
+
+    @OneToMany(mappedBy = "job")
+    List<Provider> providers;
+
 
 }
