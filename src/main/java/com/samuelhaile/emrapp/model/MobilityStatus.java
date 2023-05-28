5af72b4198c484682a735982bffd81ce9f4484f3
Samuel Haile <110855004+samhaile7@users.noreply.github.com>
2023-05-28 19:17:57 -0400

    Updated Entity relationship mbstat

--- src/main/java/com/samuelhaile/emrapp/model/MobilityStatus.java
+++ src/main/java/com/samuelhaile/emrapp/model/MobilityStatus.java
@@ -2,14 +2,51 @@ package com.samuelhaile.emrapp.model;
 
 import jakarta.persistence.*;
 
+import java.util.List;
+@Entity(name = "MobilityStatus")
 public class MobilityStatus {
 
-    @Entity(name = "Mobility Status")
-    public class Job {
+
         @Id
         @GeneratedValue(strategy = GenerationType.AUTO)
         private Long mobilityStatusId;
+
         @Column(nullable = false)
         private String statusDescription;
+
+        @OneToMany(mappedBy = "mobilityStatus")
+        private List<Patient> patients;
+
+    public MobilityStatus() {
+    }
+
+    public MobilityStatus(Long mobilityStatusId, String statusDescription, List<Patient> patients) {
+        this.mobilityStatusId = mobilityStatusId;
+        this.statusDescription = statusDescription;
+        this.patients = patients;
+    }
+
+    public Long getMobilityStatusId() {
+        return mobilityStatusId;
+    }
+
+    public void setMobilityStatusId(Long mobilityStatusId) {
+        this.mobilityStatusId = mobilityStatusId;
+    }
+
+    public String getStatusDescription() {
+        return statusDescription;
+    }
+
+    public void setStatusDescription(String statusDescription) {
+        this.statusDescription = statusDescription;
+    }
+
+    public List<Patient> getPatients() {
+        return patients;
+    }
+
+    public void setPatients(List<Patient> patients) {
+        this.patients = patients;
     }
 }
