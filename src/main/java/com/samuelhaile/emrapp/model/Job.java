588cafb12e20fe1e81e8da2a1d3019e3e202c6f0
Samuel Haile <110855004+samhaile7@users.noreply.github.com>
2023-05-28 22:45:40 -0400

    Added constructors and getters/setters

--- src/main/java/com/samuelhaile/emrapp/model/Job.java
+++ src/main/java/com/samuelhaile/emrapp/model/Job.java
@@ -15,5 +15,36 @@ public class Job {
     @OneToMany(mappedBy = "job")
     List<Provider> providers;
 
+    public Job() {
+    }
 
+    public Job(Long jobId, String jobTitle, List<Provider> providers) {
+        this.jobId = jobId;
+        this.jobTitle = jobTitle;
+        this.providers = providers;
+    }
+
+    public Long getJobId() {
+        return jobId;
+    }
+
+    public void setJobId(Long jobId) {
+        this.jobId = jobId;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
+    public List<Provider> getProviders() {
+        return providers;
+    }
+
+    public void setProviders(List<Provider> providers) {
+        this.providers = providers;
+    }
 }
