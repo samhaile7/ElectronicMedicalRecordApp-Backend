d49f6c78ee6118d68f554c7e65538c480203adb7
Samuel Haile <110855004+samhaile7@users.noreply.github.com>
2023-05-28 21:41:23 -0400

    Added patient repo

---
+++ src/main/java/com/samuelhaile/emrapp/PatientRepository.java
@@ -0,0 +1,15 @@
+package com.samuelhaile.emrapp;
+
+import com.samuelhaile.emrapp.model.Patient;
+import com.samuelhaile.emrapp.model.Provider;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PatientRepository extends JpaRepository<Patient, Long> {
+
+   // Provider getProviderByProviderById(Long providerId);
+
+   Patient findByPatientId(Long patientId);
+
+
+
+}
