bf90015cecd40c1e80ece4c18cbf163129fa3f30
Samuel Haile <110855004+samhaile7@users.noreply.github.com>
2023-05-28 22:05:30 -0400

    Finished JPA Patient controller

--- src/main/java/com/samuelhaile/emrapp/controller/PatientControllerJPA.java
+++ src/main/java/com/samuelhaile/emrapp/controller/PatientControllerJPA.java
@@ -1,73 +1,72 @@
-//package com.samuelhaile.emrapp.controller;
-//
-//import com.samuelhaile.emrapp.dao.PatientDao;
-//import com.samuelhaile.emrapp.model.Patient;
-//import com.samuelhaile.emrapp.model.RangeCheckerService;
-//import jakarta.validation.Valid;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.web.bind.annotation.*;
-//import org.springframework.web.server.ResponseStatusException;
-//
-//import java.util.List;
-//
-//@RestController
-//@CrossOrigin
-//
-//public class PatientControllerJPA {
-//
-//    private PatientDao patientDao;
-//    private RangeCheckerService rangeCheckerService = new RangeCheckerService();
-//
-//    public PatientControllerJPA(PatientDao patientDao) {
-//        this.patientDao = patientDao;
-//    }
-//
-//    @RequestMapping(path = "/test", method = RequestMethod.GET)
-//    public String getTestMessage() {
-//        return "This is a message that should display";
-//    }
-//
-//    @RequestMapping(path = "/patients", method = RequestMethod.GET)
-//    public List<Patient> listAllPatients() {
-//        return patientDao.listAllPatients();
-//    }
-//
-//
-//    @RequestMapping(path = "/patients/{id}", method = RequestMethod.GET)
-//    public Patient getPatientById(@PathVariable int id) {
-//        Patient patient = patientDao.getPatientById(id);
-//        if (patient == null) {
-//            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Patient Not Found");
-//        } else {
-//            return patient;
-//        }
-//    }
-//
-//
-//    @ResponseStatus (HttpStatus.CREATED)
-//    @RequestMapping(path = "/patients", method = RequestMethod.POST)
-//    public Patient createPatient(@RequestBody @Valid Patient newPatient) {
-//        Patient patientWithUpdatedMobilityStatus = rangeCheckerService.assignPatientMobilityStatus(newPatient);
-//        return patientDao.createPatient(patientWithUpdatedMobilityStatus);
-//    }
-//
-//    @ResponseStatus(HttpStatus.NO_CONTENT)
-//    @RequestMapping(path = "/patients/{id}", method = RequestMethod.DELETE)
-//    public void deletePatient( @PathVariable int id) {
-//        if (patientDao.getPatientById(id) == null) {
-//            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Patient Not Found");
-//        }
-//         patientDao.deletePatient(id);
-//    }
-//
-//
-//    @RequestMapping(path = "/patients", method = RequestMethod.PUT)
-//    public void updatePatient( @RequestBody @Valid  Patient patientToUpdate) {
-//        if (patientDao.getPatientById(patientToUpdate.getPatientId()) == null) {
-//            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Patient Not Found");
-//        }
-//        Patient patientWithUpdatedMobilityStatus = rangeCheckerService.assignPatientMobilityStatus(patientToUpdate);
-//        patientDao.updatePatient(patientWithUpdatedMobilityStatus);
-//    }
-//
-//}
+package com.samuelhaile.emrapp.controller;
+
+import com.samuelhaile.emrapp.PatientRepository;
+import com.samuelhaile.emrapp.ProviderRepository;
+import com.samuelhaile.emrapp.dao.PatientDao;
+import com.samuelhaile.emrapp.model.Patient;
+import com.samuelhaile.emrapp.model.RangeCheckerService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
+
+public class PatientControllerJPA {
+
+    @Autowired
+    ProviderRepository providerRepository;
+    @Autowired
+    PatientRepository patientRepository;
+    @Autowired
+    RangeCheckerService rangeCheckerService;
+
+
+    @RequestMapping(path = "/patients", method = RequestMethod.GET)
+    public List<Patient> listAllPatients() {
+        return patientRepository.findAll();
+    }
+
+
+    @RequestMapping(path = "/patients/{id}", method = RequestMethod.GET)
+    public Patient getPatientById(@PathVariable Long id) {
+        Patient patient = patientRepository.findByPatientId(id);
+        if (patient == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Patient Not Found");
+        } else {
+            return patient;
+        }
+    }
+
+
+    @ResponseStatus (HttpStatus.CREATED)
+    @RequestMapping(path = "/patients", method = RequestMethod.POST)
+    public Patient createPatient(@RequestBody @Valid Patient newPatient) {
+        Patient patientWithUpdatedMobilityStatus = rangeCheckerService.assignPatientMobilityStatus(newPatient);
+        return patientWithUpdatedMobilityStatus;
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @RequestMapping(path = "/patients/{id}", method = RequestMethod.DELETE)
+    public void deletePatient( @PathVariable Long id) {
+        if (patientRepository.findByPatientId(id) == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Patient Not Found");
+        }
+         patientRepository.delete(patientRepository.findByPatientId(id));
+    }
+
+
+    @RequestMapping(path = "/patients", method = RequestMethod.PUT)
+    public void updatePatient( @RequestBody @Valid  Patient patientToUpdate) {
+        if (patientRepository.findByPatientId(patientToUpdate.getPatientId()) == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Patient Not Found");
+        }
+        Patient patientWithUpdatedMobilityStatus = rangeCheckerService.assignPatientMobilityStatus(patientToUpdate);
+        patientRepository.save(patientWithUpdatedMobilityStatus);
+    }
+
+}
