edb3d6da94e99e1341394a8d2e6825caeaf3bccd
Samuel Haile <samhaile129@gmail.com>
2023-12-03 15:30:12 -0500

    cleanup

--- src/main/java/com/samuelhaile/emrapp/EmrApplication.java
+++ src/main/java/com/samuelhaile/emrapp/EmrApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EmrApplication {
 
 	public static void main(String[] args) {
+		System.setProperty("server.servlet.context-path", "/");
+
 		SpringApplication.run(EmrApplication.class, args);
 	}
 
