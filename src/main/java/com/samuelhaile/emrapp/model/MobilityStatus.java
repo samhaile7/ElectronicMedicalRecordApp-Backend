0924bf09f8aad9a98982dc4f1a98cf59d39955b2
Samuel Haile <samhaile129@gmail.com>
2024-07-20 20:36:23 -0400

    Add mobility status class

---
+++ src/main/java/com/samuelhaile/emrapp/model/MobilityStatus.java
@@ -0,0 +1,38 @@
+package com.samuelhaile.emrapp.model;
+
+public class MobilityStatus {
+    int mobilityStatusId;
+    String mobilityStatusDescription;
+
+    public MobilityStatus(int mobilityStatusId, String mobilityStatusDescription) {
+        this.mobilityStatusId = mobilityStatusId;
+        this.mobilityStatusDescription = mobilityStatusDescription;
+    }
+
+    public MobilityStatus() {
+    }
+
+    public int getMobilityStatusId() {
+        return mobilityStatusId;
+    }
+
+    public void setMobilityStatusId(int mobilityStatusId) {
+        this.mobilityStatusId = mobilityStatusId;
+    }
+
+    public String getMobilityStatusDescription() {
+        return mobilityStatusDescription;
+    }
+
+    public void setMobilityStatusDescription(String mobilityStatusDescription) {
+        this.mobilityStatusDescription = mobilityStatusDescription;
+    }
+
+    @Override
+    public String toString() {
+        return "MobilityStatus{" +
+                "mobilityStatusId=" + mobilityStatusId +
+                ", mobilityStatusDescription='" + mobilityStatusDescription + '\'' +
+                '}';
+    }
+}
