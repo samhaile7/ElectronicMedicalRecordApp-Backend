f5b20f4f4fc258d949e39b604ac1502200fda736
Samuel Haile <samhaile129@gmail.com>
2024-07-20 20:32:53 -0400

    Add job class

---
+++ src/main/java/com/samuelhaile/emrapp/model/Job.java
@@ -0,0 +1,31 @@
+package com.samuelhaile.emrapp.model;
+
+public class Job {
+
+    private int jobId;
+    private String jobName;
+
+    public Job(int jobId, String jobName) {
+        this.jobId = jobId;
+        this.jobName = jobName;
+    }
+
+    public Job() {
+    }
+
+    public int getJobId() {
+        return jobId;
+    }
+
+    public void setJobId(int jobId) {
+        this.jobId = jobId;
+    }
+
+    public String getJobName() {
+        return jobName;
+    }
+
+    public void setJobName(String jobName) {
+        this.jobName = jobName;
+    }
+}
