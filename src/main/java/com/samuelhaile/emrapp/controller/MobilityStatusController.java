3458569a453ae099b38920f2fdf702041029a991
Samuel Haile <samhaile129@gmail.com>
2024-08-03 11:04:26 -0400

    add msdao

--- src/main/java/com/samuelhaile/emrapp/controller/MobilityStatusController.java
+++ src/main/java/com/samuelhaile/emrapp/controller/MobilityStatusController.java
@@ -1,29 +1,29 @@
-//package com.samuelhaile.emrapp.controller;
-//
-//import com.samuelhaile.emrapp.dao.MobilityStatusDao;
-//import com.samuelhaile.emrapp.model.MobilityStatus;
-//import org.springframework.web.bind.annotation.CrossOrigin;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//import java.util.List;
-//
-//@RestController
-//@CrossOrigin
-//public class MobilityStatusController {
-//    MobilityStatusDao mobilityStatusDao;
-//    public MobilityStatusController(MobilityStatusDao mobilityStatusDao) {
-//        this.mobilityStatusDao = mobilityStatusDao;
-//    }
-//
-//    @RequestMapping(path = "/mobilitystatuses")
-//    public List<MobilityStatus> getAllMobilityStatuses() {
-//        return mobilityStatusDao.listAllMobilityStatuses();
-//    }
-//    @RequestMapping(path = "/mobilitystatus/{mobilityStatusId}")
-//    public MobilityStatus getMobilityStatusByMobilityStatusId(int mobilityStatusId) {
-//        return mobilityStatusDao.getMobilityStatusByMobilityStatusId(mobilityStatusId);
-//    }
-//
-//
-//}
+package com.samuelhaile.emrapp.controller;
+
+import com.samuelhaile.emrapp.dao.MobilityStatusDao;
+import com.samuelhaile.emrapp.model.MobilityStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
+public class MobilityStatusController {
+    MobilityStatusDao mobilityStatusDao;
+    public MobilityStatusController(MobilityStatusDao mobilityStatusDao) {
+        this.mobilityStatusDao = mobilityStatusDao;
+    }
+
+    @RequestMapping(path = "/mobilitystatus")
+    public List<MobilityStatus> getAllMobilityStatuses() {
+        return mobilityStatusDao.listAllMobilityStatuses();
+    }
+    @RequestMapping(path = "/mobilitystatus/{mobilityStatusId}")
+    public MobilityStatus getMobilityStatusByMobilityStatusId(int mobilityStatusId) {
+        return mobilityStatusDao.getMobilityStatusById(mobilityStatusId);
+    }
+
+
+}
